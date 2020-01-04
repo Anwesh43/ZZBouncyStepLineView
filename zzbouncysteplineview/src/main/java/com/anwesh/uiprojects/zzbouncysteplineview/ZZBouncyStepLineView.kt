abb80b63bbac41ae63da8e3bcb1589ddbb114382
Anwesh43 <anweshthecool0@gmail.com>
2020-01-04 16:23:46 +0530

    adding ZZBouncyStepLine class

--- zzbouncysteplineview/src/main/java/com/anwesh/uiprojects/zzbouncysteplineview/ZZBouncyStepLineView.kt
+++ zzbouncysteplineview/src/main/java/com/anwesh/uiprojects/zzbouncysteplineview/ZZBouncyStepLineView.kt
@@ -165,4 +165,28 @@ class ZZBouncyStepLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ZZBouncyStepLine(var i : Int) {
+
+        private val root : ZZBSLNode = ZZBSLNode(0)
+        private var curr : ZZBSLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
