02d84b196ebd7d966c23001ba2b19a9ab662ee1e
Anwesh43 <anweshthecool0@gmail.com>
2020-01-04 16:21:16 +0530

    adding ZZBSLNode class

--- zzbouncysteplineview/src/main/java/com/anwesh/uiprojects/zzbouncysteplineview/ZZBouncyStepLineView.kt
+++ zzbouncysteplineview/src/main/java/com/anwesh/uiprojects/zzbouncysteplineview/ZZBouncyStepLineView.kt
@@ -122,4 +122,47 @@ class ZZBouncyStepLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ZZBSLNode(var i : Int, val state : State = State()) {
+
+
+        private var next : ZZBSLNode? = null
+        private var prev : ZZBSLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ZZBSLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawZZBSLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb  : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit): ZZBSLNode {
+            var curr : ZZBSLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
