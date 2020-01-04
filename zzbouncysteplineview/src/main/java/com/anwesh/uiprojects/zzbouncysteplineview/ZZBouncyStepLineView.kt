15299db0a437d9e0dc7fcdc546f98f1fabe7d1db
Anwesh43 <anweshthecool0@gmail.com>
2020-01-04 15:39:37 +0530

    adding Int and Float extension methods

--- zzbouncysteplineview/src/main/java/com/anwesh/uiprojects/zzbouncysteplineview/ZZBouncyStepLineView.kt
+++ zzbouncysteplineview/src/main/java/com/anwesh/uiprojects/zzbouncysteplineview/ZZBouncyStepLineView.kt
@@ -20,3 +20,8 @@ val foreColor : Int = Color.parseColor("#9C27B0")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 30
 val scGap : Float = 0.02f / lines
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
